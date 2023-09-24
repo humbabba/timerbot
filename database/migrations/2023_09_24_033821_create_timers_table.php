a1be2435ba8508d3cdf162df99e94394aa85654c
humbabba <humbabba@gmail.com>
2023-09-24 08:17:00 -0700

    Adds timers table migration

---
+++ database/migrations/2023_09_24_033821_create_timers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTimersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('guys')->default(8);
+            $table->time('end_time')->default('20:48:00');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timers');
+    }
+}
